59bbf8c11d3bfc980fc27f850608d17512bb5284
Lihang Zhou <zhou.liha@husky.neu.edu>
2019-10-27 21:45:24 -0400

    update some methods

--- lab7/src/lab7/analytics/AnalysisHelper.java
+++ lab7/src/lab7/analytics/AnalysisHelper.java
@@ -53,65 +53,67 @@ public class AnalysisHelper {
 
     // find 5 comments which have the most likes
     // TODO
-    public void getFiveMostLikedComment(){
-        Map<Integer,Comment> comments=DataStore.getInstance().getComments();
-        List<Comment> commentList=new ArrayList<>(comments.values());
-        Collections.sort(commentList,new Comparator<Comment>(){
+    public void getFiveMostLikedComment() {
+        Map<Integer, Comment> comments = DataStore.getInstance().getComments();
+        List<Comment> commentList = new ArrayList<>(comments.values());
+        Collections.sort(commentList, new Comparator<Comment>() {
             @Override
             public int compare(Comment o1, Comment o2) {
                 //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                return o2.getLikes()-o1.getLikes();
+                return o2.getLikes() - o1.getLikes();
             }
-            
+
         });
-        
+
         System.out.println("5 most likes comments: ");
-        for(int i=0;i<commentList.size()&&i<5;i++){
+        for (int i = 0; i < commentList.size() && i < 5; i++) {
             System.out.println(commentList.get(i));
         }
     }
-    
+
     // find Average number of likes per comment
-    public void getAvgLikeNumPerComment(){
-        Map<Integer,Comment> comments=DataStore.getInstance().getComments();
-        List<Comment> commentList=new ArrayList<>(comments.values());
-        double likes=0;
-        for(Comment c:commentList){
-            likes+=c.getLikes();
+    public void getAvgLikeNumPerComment() {
+        Map<Integer, Comment> comments = DataStore.getInstance().getComments();
+        List<Comment> commentList = new ArrayList<>(comments.values());
+        double likes = 0;
+        for (Comment c : commentList) {
+            likes += c.getLikes();
         }
-        likes=likes/commentList.size();
-        
-        System.out.println("Average # of Likes Per Comment: "+likes);
+        likes = likes / commentList.size();
+
+        System.out.println("Average # of Likes Per Comment: " + likes);
     }
+
     // Find the post with most liked comments.
-    public void getPostWithMostLikedComments(){
-        Map<Integer,Comment> commentMap=DataStore.getInstance().getComments();
-        List<Comment> comments=new ArrayList<>(commentMap.values());
-        Collections.sort(comments,new Comparator<Comment>(){
+    public void getPostWithMostLikedComments() {
+        Map<Integer, Comment> commentMap = DataStore.getInstance().getComments();
+        List<Comment> comments = new ArrayList<>(commentMap.values());
+        Collections.sort(comments, new Comparator<Comment>() {
             @Override
             public int compare(Comment o1, Comment o2) {
-                return o2.getLikes()-o1.getLikes();
+                return Integer.compare(o2.getLikes(), o1.getLikes());
             }
         });
-        Comment mlc=comments.get(0);
-        Map<Integer,Post> postMap=DataStore.getInstance().getPosts();
-        Post post=postMap.get(mlc.getPostId());
-        
-        System.out.println("Post w/ Most Liked Comments: "+post);
+        Comment mlc = comments.get(0);
+        Map<Integer, Post> postMap = DataStore.getInstance().getPosts();
+        Post post = postMap.get(mlc.getPostId());
+
+        System.out.println("Post w/ Most Liked Comments: " + post);
     }
+
     // Find the post with most comments
-    public void getPostWithMostComments(){
-        Map<Integer,Post> postMap=DataStore.getInstance().getPosts();
-        List<Post> posts=new ArrayList<>(postMap.values());
+    public void getPostWithMostComments() {
+        Map<Integer, Post> postMap = DataStore.getInstance().getPosts();
+        List<Post> posts = new ArrayList<>(postMap.values());
         Collections.sort(posts, new Comparator<Post>() {
             @Override
             public int compare(Post o1, Post o2) {
-                return o2.getComments().size()-o1.getComments().size();
+                return Integer.compare(o2.getComments().size(), o1.getComments().size());
             }
         });
-        Post pwmc=posts.get(0);
-        
-        System.out.println("Post w/ Most Comments ("+pwmc.getComments().size()+"): "+pwmc);
+        Post pwmc = posts.get(0);
+
+        System.out.println("Post w/ Most Comments (" + pwmc.getComments().size() + "): " + pwmc);
     }
-        
+
 }
