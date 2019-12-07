aea770f48ef1f42f16516e8ae9aef2c244a1bf5b
Jieying Chen <chen.jiey@husky.neu.edu>
2019-12-07 13:27:55 -0500

    small changes

--- Thursday_Final/src/com/thursday/business/CompanyDirectory.java
+++ Thursday_Final/src/com/thursday/business/CompanyDirectory.java
@@ -67,4 +67,8 @@ public class CompanyDirectory {
     public static List<Company> getAllCompanies() {
         return CompanyBiz.getAllCompanies();
     }
+    
+    public static boolean checkCompanyExistance(String name) {
+        return getCompany(name) != null;
+    }
 }
